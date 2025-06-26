833b9a68a4b273507396517a6feff9615c70c62f
sjuma <sheillajuma001@gmail.com>
2025-06-26 14:14:16 +0300

    Add cart quantity editing functionality
    - Add update_quantity action to carts controller
    - Allow users to change item quantities in cart
    - Automatically remove items if quantity is set to 0
    - Update redirects to use shop_path instead of root_path
    - Provides better cart management user experience

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:show, :destroy]
-  before_action :set_cart_item, only: [:add_item, :remove_item]
+  before_action :set_cart_item, only: [:add_item, :remove_item, :update_quantity]
 
   def show
     @cart_items = @cart.cart_items.includes(:product)
@@ -12,9 +12,9 @@ class CartsController < ApplicationController
     @cart_item = current_cart.add_product(product)
 
     if @cart_item.save
-      redirect_to request.referer || root_path, notice: 'Added to your cart'
+      redirect_to request.referer || shop_path, notice: 'Added to your cart'
     else
-      redirect_to request.referer || root_path, alert: 'Unable to add item to cart'
+      redirect_to request.referer || shop_path, alert: 'Unable to add item to cart'
     end
   end
 
@@ -26,7 +26,20 @@ class CartsController < ApplicationController
 
   def empty_cart
     current_cart.empty_cart
-    redirect_to root_path, notice: 'Cart emptied successfully'
+    redirect_to shop_path, notice: 'Cart emptied successfully'
+  end
+
+  def update_quantity
+    @cart_item = current_cart.cart_items.find(params[:id])
+    new_quantity = params[:quantity].to_i
+
+    if new_quantity > 0
+      @cart_item.update(quantity: new_quantity)
+      redirect_to cart_path(current_cart), notice: 'Quantity updated'
+    else
+      @cart_item.destroy
+      redirect_to cart_path(current_cart), notice: 'Item removed from cart'
+    end
   end
 
   private
