9f6d0dcca59d3f9ca1cabfc4d51727a07a50d3b0
sjuma <sheillajuma001@gmail.com>
2025-06-26 13:01:51 +0300

    Add cart routes for shopping cart functionality
    Routes added:
    - GET /carts/:id - Show cart contents
    - DELETE /carts/:id/empty_cart - Empty entire cart
    - POST /cart/add_item/:product_id - Add item to cart
    - DELETE /cart/remove_item/:id - Remove item from cart
    
    Provides RESTful routes for all cart operations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+
+  resources :carts, only: [:show] do
+    member do
+      delete :empty_cart
+    end
+  end
+
+  post 'cart/add_item/:product_id', to: 'carts#add_item', as: 'add_to_cart'
+  delete 'cart/remove_item/:id', to: 'carts#remove_item', as: 'remove_from_cart'
+
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
