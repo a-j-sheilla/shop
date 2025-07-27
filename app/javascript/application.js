57bfb9b0f324b2beeb1ca0830a08e5ada6855a82
vmuhembe <muhembelev@gmail.com>
2025-07-28 00:18:35 +0300

    refactore: change the posion of js files

---
+++ app/javascript/application.js
@@ -0,0 +1,20 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+console.log("Application JavaScript loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".global-notification").forEach(msg => {
+    setTimeout(() => {
+      msg.style.transition = "opacity 1s ease-out";
+      msg.style.opacity = 0;
+      setTimeout(() => msg.remove(), 500);
+    }, 100);
+  });
+});
