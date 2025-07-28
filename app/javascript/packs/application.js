2269bc048f32133e3d9b71a69cb9055d358734bd
vmuhembe <muhembelev@gmail.com>
2025-07-28 12:27:52 +0300

    refactor: use previously provided  js fies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,14 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+// app/javascript/packs/application.js
+
+import "../channels"
+import "../global"
+import "../products"
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-console.log("Application JavaScript loaded");
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".global-notification").forEach(msg => {
-    setTimeout(() => {
-      msg.style.transition = "opacity 1s ease-out";
-      msg.style.opacity = 0;
-      setTimeout(() => msg.remove(), 4000);
-    }, 100);
-  });
-});
