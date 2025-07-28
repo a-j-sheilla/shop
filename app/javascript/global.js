03832b39601c36a8e1c45110f95c9031a79e3b5a
vmuhembe <muhembelev@gmail.com>
2025-07-28 12:53:32 +0300

    fix: let right side menu to show on clicking the hamburger

--- app/javascript/global.js
+++ app/javascript/global.js
@@ -11,3 +11,20 @@ document.addEventListener("turbolinks:load", function() {
   }
 
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const burgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if (burgers.length > 0) {
+    burgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const targetId = el.dataset.target;
+        const target = document.getElementById(targetId);
+
+        el.classList.toggle('is-active');
+        target.classList.toggle('is-active');
+      });
+    });
+  }
+});
